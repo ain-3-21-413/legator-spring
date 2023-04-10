e443373ae7703b990c546aa1ebbee6b5169e6905
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-10 21:42:30 +0600

    init

---
+++ src/main/java/kg/inai/legator/LegatorApplication.java
@@ -0,0 +1,13 @@
+package kg.inai.legator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/inai/legator/LegatorApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.inai.legator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
