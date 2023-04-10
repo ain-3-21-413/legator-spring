db4c207f301d9608e0f3abf5087ad792869cf25d
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-10 21:53:33 +0600

    feat: turn user to user details

--- src/main/java/kg/inai/legator/entity/User.java
+++ src/main/java/kg/inai/legator/entity/User.java
@@ -1,5 +1,12 @@
 package kg.inai.legator.entity;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -22,7 +29,7 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @Builder
 @Entity
-public class User {
+public class User implements UserDetails {
 
 	@GeneratedValue(strategy = GenerationType.TABLE)
 	@Id
@@ -34,4 +41,29 @@ public class User {
 
 	@Enumerated(EnumType.STRING)
 	ERole role;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
