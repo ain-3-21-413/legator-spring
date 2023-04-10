be18c99f778851e9944aa3590800fbda371bd802
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-10 22:14:31 +0600

    feat: add security confg

---
+++ src/main/java/kg/inai/legator/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package kg.inai.legator.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import kg.inai.legator.jwt.AuthEntryPoint;
+import kg.inai.legator.jwt.JwtFilter;
+
+@RequiredArgsConstructor
+@EnableMethodSecurity
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/v3/api-docs/**",
+    };
+
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtFilter jwtFilter;
+    private final AuthEntryPoint authEntryPoint;
+
+    @Bean
+    public SecurityFilterChain mySecurityFilterChain(final HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authCustomizer -> authCustomizer
+                        .requestMatchers(AUTH_WHITELIST).permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling().authenticationEntryPoint(authEntryPoint)
+                .and().authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
