ed64b63009ab55c773d71a1f574c481cc34e780a
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-10 22:33:33 +0600

    feat: add rest controller advice

---
+++ src/main/java/kg/inai/legator/advice/RestApiExceptionHandler.java
@@ -0,0 +1,38 @@
+package kg.inai.legator.advice;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import kg.inai.legator.exception.RoleNotFoundException;
+import kg.inai.legator.exception.UserNotFoundException;
+import kg.inai.legator.exception.UsernameTakenException;
+import kg.inai.legator.model.ErrorMessage;
+
+
+@RestControllerAdvice
+public class RestApiExceptionHandler {
+
+
+    @ExceptionHandler({UserNotFoundException.class, RoleNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorMessage notFoundException(Exception exception, WebRequest request) {
+        return new ErrorMessage(
+                exception.getMessage(),
+                new Date()
+        );
+    }
+
+    @ExceptionHandler(UsernameTakenException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorMessage usernameTakenException(Exception exception, WebRequest request) {
+        return new ErrorMessage(
+                exception.getMessage(),
+                new Date()
+        );
+    }
+}
