05f402ddd2f258f605bfdc3800dd97425baeb3fe
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-10 21:52:27 +0600

    feat: make user entity

--- src/main/java/kg/inai/legator/entity/User.java
+++ src/main/java/kg/inai/legator/entity/User.java
@@ -1,14 +1,37 @@
 package kg.inai.legator.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import kg.inai.legator.enums.ERole;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class User {
 
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	@Id
 	Long id;
 
 	String username;
 
 	String password;
 
+	@Enumerated(EnumType.STRING)
 	ERole role;
 }
