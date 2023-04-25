f17083391acc9b95aea63976b79fd26707616512
diana1033 <dmaratova18@gmail.com>
2023-04-25 12:20:34 +0600

    bug:fix item record add bibliographic record

--- src/main/java/kg/inai/legator/entity/item/ItemRecord.java
+++ src/main/java/kg/inai/legator/entity/item/ItemRecord.java
@@ -1,9 +1,13 @@
 package kg.inai.legator.entity.item;
 
+import kg.inai.legator.entity.bibliographicrecord.BibliographicRecord;
 import kg.inai.legator.entity.patron.Patron;
+import lombok.experimental.FieldDefaults;
 
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class ItemRecord {
     String number;
+    BibliographicRecord bibliographicRecord;
     ItemRecordStatus itemRecordStatus;
     AccessLevel accessLevel;
     Patron patron;
