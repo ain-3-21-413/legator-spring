beb772c5cb2c00f25a3d477f87ed3d63922e79c4
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-25 20:24:36 +0600

    feat: add access level entity

--- src/main/java/kg/inai/legator/entity/item/AccessLevel.java
+++ src/main/java/kg/inai/legator/entity/item/AccessLevel.java
@@ -1,9 +1,28 @@
 package kg.inai.legator.entity.item;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
+@Getter
+@Setter
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class AccessLevel {
+
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Id
     int id;
+    
     int level;
 }
