ce5fd5e9ab77a9493b71dc050c8b4930780eab93
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-25 20:50:50 +0600

    feat: add ItemTransactionType, and ItemTransaction entities

--- src/main/java/kg/inai/legator/entity/itemtransaction/ItemTransaction.java
+++ src/main/java/kg/inai/legator/entity/itemtransaction/ItemTransaction.java
@@ -3,16 +3,55 @@ package kg.inai.legator.entity.itemtransaction;
 import kg.inai.legator.entity.Librarian;
 import kg.inai.legator.entity.item.ItemRecord;
 import kg.inai.legator.entity.patron.Patron;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
-import java.util.Date;
+import java.sql.Date;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Getter
+@Setter
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class ItemTransaction {
-    int id;
+
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Id
+    Long id;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "patron_id")
     Patron patron;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "librarian_id")
     Librarian librarian;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "item_record_id")
     ItemRecord itemRecord;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "item_transaction_type_id")
     ItemTransactionType itemTransactionType;
+    
     Date issuesAt;
+    
     Date expectedEnd;
+    
     Date actualEnd;
 }
--- src/main/java/kg/inai/legator/entity/itemtransaction/ItemTransactionType.java
+++ src/main/java/kg/inai/legator/entity/itemtransaction/ItemTransactionType.java
@@ -1,7 +1,28 @@
 package kg.inai.legator.entity.itemtransaction;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class ItemTransactionType {
 
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Id
     int id;
+    
     String name;
 }
