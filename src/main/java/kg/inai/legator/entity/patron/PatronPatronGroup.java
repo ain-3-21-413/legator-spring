2ea5ca341b8b383910b5c812ddce4a9a89c59898
diana1033 <dmaratova18@gmail.com>
2023-04-25 11:50:28 +0600

    bug:fix patron patron group, patron, add access level

--- src/main/java/kg/inai/legator/entity/patron/PatronPatronGroup.java
+++ src/main/java/kg/inai/legator/entity/patron/PatronPatronGroup.java
@@ -1,5 +1,10 @@
 package kg.inai.legator.entity.patron;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.parameters.P;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class PatronPatronGroup {
     Patron patron;
     PatronGroup patronGroup;
