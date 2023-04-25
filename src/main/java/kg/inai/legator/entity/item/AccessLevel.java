ccbba801c461122988dec545970195c5b511ff0b
diana1033 <dmaratova18@gmail.com>
2023-04-25 11:31:30 +0600

    feat: add access level, item record, item record status

---
+++ src/main/java/kg/inai/legator/entity/item/AccessLevel.java
@@ -0,0 +1,9 @@
+package kg.inai.legator.entity.item;
+
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class AccessLevel {
+    int id;
+    int level;
+}
---
+++ src/main/java/kg/inai/legator/entity/item/ItemRecord.java
@@ -0,0 +1,10 @@
+package kg.inai.legator.entity.item;
+
+import kg.inai.legator.entity.patron.Patron;
+
+public class ItemRecord {
+    String number;
+    ItemRecordStatus itemRecordStatus;
+    AccessLevel accessLevel;
+    Patron patron;
+}
