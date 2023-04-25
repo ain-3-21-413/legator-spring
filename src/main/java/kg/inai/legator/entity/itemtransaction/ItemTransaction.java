c656ec9daf2707cb619af9bbfccca42d6f9f4006
kanaevbaytik <kanaevbaytik@gmail.com>
2023-04-25 11:45:40 +0600

    feat: add Librarian, Transaction, TransactionType

--- src/main/java/kg/inai/legator/entity/itemtransaction/ItemTransaction.java
+++ src/main/java/kg/inai/legator/entity/itemtransaction/ItemTransaction.java
@@ -1,6 +1,7 @@
 package kg.inai.legator.entity.itemtransaction;
 
 import kg.inai.legator.entity.Librarian;
+import kg.inai.legator.entity.item.ItemRecord;
 import kg.inai.legator.entity.patron.Patron;
 
 import java.util.Date;
@@ -9,6 +10,7 @@ public class ItemTransaction {
     int id;
     Patron patron;
     Librarian librarian;
+    ItemRecord itemRecord;
     ItemTransactionType itemTransactionType;
     Date issuesAt;
     Date expectedEnd;
