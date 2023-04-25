0eb4fdc5040fa551e299f3b731242a8a0526e040
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-25 20:49:34 +0600

    feat: add librarian entity

--- src/main/java/kg/inai/legator/entity/Librarian.java
+++ src/main/java/kg/inai/legator/entity/Librarian.java
@@ -1,12 +1,40 @@
 package kg.inai.legator.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Librarian {
 
+    @Id
     String number;
+    
     String firstName;
+    
     String lastName;
+    
     String middleName;
+    
     String phone;
+    
     String email;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
     User user;
 }
