0df78ca568919875135999a5bada6de8b6c3afe9
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-25 20:25:40 +0600

    feat: add Patron, PatronGroup entities and relation between them

--- src/main/java/kg/inai/legator/entity/patron/Patron.java
+++ src/main/java/kg/inai/legator/entity/patron/Patron.java
@@ -1,17 +1,46 @@
 package kg.inai.legator.entity.patron;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import kg.inai.legator.entity.User;
 import kg.inai.legator.entity.item.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
+@Getter
+@Setter
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Patron {
+
+    @Id
     String number;
+    
     String firstName;
+    
     String lastName;
+    
     String middleName;
+    
     String phone;
+    
     String email;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "access_level_id")
     AccessLevel accessLevel;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
     User user;
 }
--- src/main/java/kg/inai/legator/entity/patron/PatronPatronGroup.java
+++ src/main/java/kg/inai/legator/entity/patron/PatronPatronGroup.java
@@ -1,10 +1,37 @@
 package kg.inai.legator.entity.patron;
 
-import lombok.AccessLevel;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class PatronPatronGroup {
+
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Id
+    Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "patron_id")
     Patron patron;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "patron_group_id")
     PatronGroup patronGroup;
 }
