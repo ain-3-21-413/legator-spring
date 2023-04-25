63871b54d5a56686c3d21f7f18bc5d41e889cad9
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-25 20:27:06 +0600

    feat: add repositories for Patron, PatronGroup, PatronPatronGroup, and AccessLevel

---
+++ src/main/java/kg/inai/legator/repository/patron/PatronRepository.java
@@ -0,0 +1,11 @@
+package kg.inai.legator.repository.patron;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import kg.inai.legator.entity.patron.Patron;
+
+@Repository
+public interface PatronRepository extends JpaRepository<Patron, String> {
+
+}
